04706e6cec83d04da33d3af83d67480032af92f0
saullfeng <2544458760@qq.com>
2022-08-29 15:13:32 +0800

    rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::{get,launch,routes};
+
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+#[get("/")]
+fn index()->&'static str{
+   " Hello, world!"
+}
+
+#[launch]
+fn rocket()-> _ {
+    rocket::build().mount("/",routes![index])
 }
